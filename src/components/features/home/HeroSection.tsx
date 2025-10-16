a0c6d5a0a8ce34de1bd70a60912d13f2ebcddf05
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2025-10-16 22:59:49 +0100

    hero section done

--- src/components/features/home/HeroSection.tsx
+++ src/components/features/home/HeroSection.tsx
@@ -5,7 +5,7 @@ import {
   type CarouselApi,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
@@ -35,55 +35,65 @@ export default function HeroSection() {
       title: "shop local, live better",
       description: "Quality nearby, always.",
       image:
-        "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1964",
-      gradientColour: "bg-green-700",
+        "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?auto=format&fit=crop&q=80&w=1964",
+      gradient: { from: "from-primary-dark", to: "to-primary-light" },
+      buttonColor: "#33B36B",
     },
     {
       title: "smart tech, near you",
       description: "Latest gadgets from trusted local stores.",
       image:
-        "https://images.unsplash.com/photo-1580137189272-c9379f8864fd?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070",
-      gradientColour: "bg-orange-700",
+        "https://images.unsplash.com/photo-1580137189272-c9379f8864fd?auto=format&fit=crop&q=80&w=2070",
+      gradient: { from: "from-secondary-dark", to: "to-secondary-light" },
+      buttonColor: "#FFB878",
     },
     {
       title: "style, right next door",
       description: "Trendy outfits from nearby stores.",
       image:
-        "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070",
-      gradientColour: "bg-blue-700",
+        "https://images.unsplash.com/photo-1541701494587-cb58502866ab?auto=format&fit=crop&q=80&w=2070",
+      gradient: { from: "from-blue-dark", to: "to-blue-light" },
+      buttonColor: "#1c6fc1",
     },
   ];
 
   const { ArrowRightIcon } = Icons;
 
+  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
+
   return (
     <div aria-label="Hero Section">
       <Carousel
-        plugins={[
-          Autoplay({
-            delay: 2000,
-          }),
-        ]}
+        plugins={[plugin.current]}
+        onMouseEnter={() => plugin.current.stop()}
+        onMouseLeave={() => plugin.current.play()}
         setApi={setApi}
         className="relative w-full"
       >
         <CarouselContent>
           {heroDisplay.map(
-            ({ title, description, image, gradientColour }, i) => (
+            ({ title, description, image, gradient, buttonColor }, i) => (
               <CarouselItem key={i}>
                 <Card className="grid min-h-[calc(100dvh-120px)] rounded-none border-none p-0">
                   <CardContent
-                    className={`${gradientColour} flex flex-col gap-2 px-0 sm:flex-row`}
+                    className={cn(
+                      "flex flex-col gap-2 bg-gradient-to-br px-0 sm:flex-row",
+                      gradient.to,
+                      gradient.from,
+                    )}
                   >
                     <div className="my-auto w-full space-y-10 pl-12">
                       <div className="space-y-4 text-white">
                         <p>{description}</p>
-                        <h1 className="w-[61%] text-[3.5rem] font-bold capitalize">
+                        <h1 className="w-[61%] text-[3.5rem] leading-16 font-bold capitalize">
                           {title}
                         </h1>
                       </div>
 
-                      <Button className="bg-primary-light group hover:bg-primary-dark cursor-pointer p-6 text-white capitalize transition-all duration-300 active:scale-105">
+                      <Button
+                        style={{ backgroundColor: buttonColor }}
+                        className="group cursor-pointer p-6 text-white capitalize shadow-xs transition-all duration-300 hover:opacity-90 active:scale-105"
+                      >
                         shop now
                         <span>
                           <ArrowRightIcon className="duration-300 group-hover:-rotate-25" />
@@ -94,7 +104,7 @@ export default function HeroSection() {
                     <div className="w-full">
                       <img
                         src={image}
-                        alt="image name"
+                        alt={title}
                         className="block size-full object-cover"
                       />
                     </div>
@@ -112,9 +122,12 @@ export default function HeroSection() {
               size={"icon-sm"}
               key={i}
               onClick={() => api?.scrollTo(i)}
-              className={cn("size-3 rounded-full bg-white/50", {
-                "bg-white": current === i + 1,
-              })}
+              className={cn(
+                "size-3 cursor-pointer rounded-full bg-white/50 hover:bg-white/70",
+                {
+                  "bg-white": current === i + 1,
+                },
+              )}
             />
           ))}
         </div>
